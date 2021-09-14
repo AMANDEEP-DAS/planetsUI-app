429b03c83f8b87957224e5c2905f26f3f8bf14fb
AMANDEEP-DAS <dasaman1997@gmail.com>
2021-09-14 11:42:10 +0530

    login screen in progress

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:planets_app/screens/login_screen.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: LoginScreen(),
+));
